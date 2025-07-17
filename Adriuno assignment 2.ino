f5d06aaaf588355dd66c71dca07c854d62f837c2
Agbogla Elorm Kwame <kwameelorm2007@gmail.com>
2025-07-17 15:32:08 +0000

    Add files via upload

---
+++ Adriuno assignment 2.ino
@@ -0,0 +1,16 @@
+const int potPin = 12:
+void 
+setup() {
+  Serial.begin(115200);
+  }
+
+void 
+loop() {
+  int potValue = analogRead(potPin);
+
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
+
+  delay(200);
+
+}
